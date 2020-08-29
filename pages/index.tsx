3624f4a1bd38ca25c6d3da8b503fe91dcbc2d1e1
RaiderII <ramiroherrera.utn@gmail.com>
2020-08-29 14:33:36 -0300

    testing filteredPoke display

--- pages/index.tsx
+++ pages/index.tsx
@@ -47,7 +47,7 @@ export default function Homepage({ pokemons, userName, myPokemon }: HomeProps): 
 
   const filteredPoke = pokemons
     .map((poke) => {
-      return poke.name.slice(0, 'char'.length) === 'char' && poke.name;
+      return poke.name.slice(0, name.length) === 'char' && poke.name;
     })
     .filter((poke) => poke);
 
@@ -103,6 +103,14 @@ export default function Homepage({ pokemons, userName, myPokemon }: HomeProps): 
               searchRef.current.focus();
             }}
           />
+          {name}
+          {filteredPoke.map((poke) => {
+            return (
+              <ul>
+                <li>{poke}</li>
+              </ul>
+            );
+          })}
 
           <List search={search}>
             {filteredPoke.map((poke) => {
