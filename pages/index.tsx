ab452d35b12e18116f4e2fba6f3a62301b2130a5
RaiderII <ramiroherrera.utn@gmail.com>
2020-08-29 15:04:07 -0300

    = and tostring

--- pages/index.tsx
+++ pages/index.tsx
@@ -63,6 +63,8 @@ export default function Homepage({ pokemons, userName, myPokemon }: HomeProps): 
 
   console.log(scroll);
 
+  //comment
+
   const dispatch: AppDispatch = useDispatch();
   useEffect(() => {
     if (store.length < 150) {
