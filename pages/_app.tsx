ea5e30c1854f9efde5855455abec1402afc21c49
RaiderII <ramiroherrera.utn@gmail.com>
2020-08-29 12:30:08 -0300

    testing disable drag on iphone

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -26,7 +26,7 @@ const MyApp = ({ Component, pageProps }) => {
           />
           <meta
             name="viewport"
-            content="width=device-width, user-scalable=no, maximum-scale=1.0, initial-scale=1"
+            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
           />
         </Head>
 
