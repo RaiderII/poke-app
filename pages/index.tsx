4e3b99db35d5f87b48cd57092ed9a48d8e90ddc5
RaiderII <ramiroherrera.utn@gmail.com>
2020-09-01 10:41:01 -0300

    removed filter

--- pages/index.tsx
+++ pages/index.tsx
@@ -51,11 +51,9 @@ export default function Homepage({ pokemons, userName, myPokemon, redirect }: Ho
 
   const searchRef = useRef(null);
 
-  const filteredPoke = pokemons
-    .map((poke) => {
-      return poke.name.slice(0, name.length) === name && poke.name;
-    })
-    .filter((poke) => poke);
+  const filteredPoke = pokemons.map((poke) => {
+    return poke.name.slice(0, name.length) === name && poke.name;
+  });
 
   console.log('filteredPoke', filteredPoke);
   useEffect(() => {
