06425902e091a0dd322791fcde586c565ba01f50
RaiderII <ramiroherrera.utn@gmail.com>
2020-09-01 14:07:09 -0300

    testing gSSP HOC

--- pages/index.tsx
+++ pages/index.tsx
@@ -169,25 +169,6 @@ function getApiData() {
   return async (ctx) => {
     const cookie = parseCookies(ctx).autho;
 
-    if (!cookie) {
-      ctx.res.writeHead(302, { Location: '/login' });
-      ctx.res.end();
-      return { props: {} };
-    }
-
-    const validQuery = {
-      text: 'SELECT fk_users_id FROM tokens WHERE token = $1 AND status = true',
-      values: [cookie],
-    };
-    const isValidUser = await db.query(validQuery);
-
-    // no user with valid token is found
-    if (isValidUser.rows.length === 0) {
-      ctx.res.writeHead(302, { Location: '/login' });
-      ctx.res.end();
-      return { props: {} };
-    }
-
     const query = {
       text: 'SELECT fk_users_id FROM tokens WHERE token = $1 AND status = true',
       values: [cookie],
